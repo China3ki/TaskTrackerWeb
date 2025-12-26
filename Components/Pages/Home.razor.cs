5fae500256ebe955a6a5e10dadb6531e3b91b541
China3ki <limon4007@gmail.com>
2025-12-26 11:27:50 +0100

    First commit

---
+++ Components/Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace TaskTrackerWeb.Components.Pages
+{
+    public partial class Home
+    {
+        private Dictionary<string, bool> _showDesc = new() { { "Plan", false }, { "Track", false }, { "Achieve", false } };
+        //private bool _showDesc = false;
+        private void HandleDesc(string name)
+        {
+            foreach(var item in _showDesc)
+            {
+                if (name != item.Key) _showDesc[item.Key] = false;
+                else _showDesc[item.Key] = _showDesc[item.Key] == false; 
+            }
+        }
+    }
+}
